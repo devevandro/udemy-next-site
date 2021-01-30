fda8e4226294bea415a7ab3d2724722cb9d348e0
Evandro Ferreira <dev.evandro@gmail.com>
2021-01-30 15:50:19 -0300

    final

---
+++ components/navigation/index.tsx
@@ -0,0 +1,45 @@
+import React, { ReactNode } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import styles from './navigation.module.css';
+
+const LINK = [
+    {
+        name: 'Home',
+        path: '/'
+    },
+
+    {
+        name: 'About',
+        path: '/about'
+    }
+];
+
+type NavAnchor = {
+    path: string
+    children: ReactNode
+};
+
+function NavAnchor ({path, children}: NavAnchor) {
+    return (<>
+        <Link href={path}>
+            <a className={styles.navAnchor}>{children}</a>
+        </Link>
+    </>);
+}
+
+export default function Navigation() {
+    const { pathname } = useRouter();
+
+    return (<>
+        <nav>
+            <ul className={styles.list}>
+                {LINK.map(({name, path}) => (
+                    <li key={path}>
+                        {path === pathname ? <span>{name}</span> : <NavAnchor path={path}>{name}</NavAnchor>}
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    </>);
+}
